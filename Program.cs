0183c762187833aee74a6c18f8d8935c5ba2be95
hardi.majmundar <hardimajmundar638@gmail.com>
2018-06-27 17:30:17 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application10
+{
+    class mainfuncaion
+    {
+        public static int Count(string x)
+        {
+            int result = 0;
+
+            x = x.Trim();
+            if (x == "")
+                return 0;
+            while (x.Contains("  "))
+                x = x.Replace("  ", " ");
+            foreach (string y in x.Split(' '))
+                result++;
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            string x = "";
+            Console.WriteLine("Enter the String:");
+            x = Console.ReadLine();
+            mainfuncaion mf = new mainfuncaion();
+            mf.Count(x);
+            Console.ReadKey();
+        }
+    }
+}
